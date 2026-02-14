5628bf570cb0b65bbb023c164df8fef8fa72e656
tj-commits <thefoxjones@gmail.com>
2026-02-14 14:52:53 -0600

    headhurts

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var composeFunction = require("compose-function")
 var flatmap = require("array.prototype.flatmap")
+var $apply = require("function.apply-x")
+var arrayWrap = require("as-array")
+var times = require("lodash.times")
+var numberTwo = require("es-two")
+var constant = require("literally")
 
 module.exports = function twiceCallWrapper(fn) {
-  return composeFunction(...flatmap([fn], (item) => [item, item]))
+  return $apply.call(
+    composeFunction,
+    require("empty-module"),
+    flatmap(arrayWrap(fn), (item) => times(numberTwo, constant(item)))
+  )
 }
