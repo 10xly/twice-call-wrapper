60a3564a300f01430f9503dc650246a999c52f21
tj-commits <thefoxjones@gmail.com>
2026-03-13 09:12:29 -0500

    refactor code to make ESLint pass, and depend on call-bind-enterprise

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-var */
+
 var composeFunction = require("compose-function")
 var flatmap = require("array.prototype.flatmap")
 var $apply = require("function.apply-x")
@@ -5,9 +7,12 @@ var arrayWrap = require("as-array")
 var times = require("lodash.times")
 var numberTwo = require("es-two")
 var constant = require("literally")
+var call = require("call-bind-enterprise/lib/callBoundCall")
 
+// eslint-disable-next-line unicorn/prevent-abbreviations
 module.exports = function twiceCallWrapper(fn) {
-  return $apply.call(
+  return call(
+    $apply,
     composeFunction,
     require("empty-module"),
     flatmap(arrayWrap(fn), (item) => times(numberTwo, constant(item)))
