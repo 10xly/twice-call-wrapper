5f0a1105c3e1e7669b55a621c5eef5fa4a80d561
tj-commits <thefoxjones@gmail.com>
2026-04-08 10:42:23 -0500

    update code to pass @10xly/eslint-config 1.0.8

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ var composeFunction = require("compose-function")
 var flatmap = require("array.prototype.flatmap")
 var $apply = require("function.apply-x")
 var arrayWrap = require("as-array")
-var times = require("lodash.times")
+var times = require("lolite.__private.multiplyfallback")
 var numberTwo = require("es-two")
 var constant = require("literally")
 var call = require("call-bind-enterprise/src/callBoundCall")
+var nullvalue = require("empty-module")
 
 // eslint-disable-next-line unicorn/prevent-abbreviations
 module.exports = function twiceCallWrapper(fn) {
   return call(
     $apply,
     composeFunction,
-    require("empty-module"),
-    flatmap(arrayWrap(fn), (item) => times(numberTwo, constant(item)))
+    nullvalue,
+    flatmap(arrayWrap(fn), item => times(numberTwo, constant(item))),
   )
 }
