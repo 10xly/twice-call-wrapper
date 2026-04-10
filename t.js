90ac7b8aab32c4e87e54df3e0eab3014aac1f902
tj-commits <thefoxjones@gmail.com>
2026-04-10 09:35:38 -0500

    oopsydaisy

--- t.js
+++
@@ -1,9 +0,0 @@
-var calledtwice = require('.');
-
-
-
-var double = v => v * 2; // a simple function that doubles a number
-
-var doubleTwice = calledtwice(double);
-
-console.log(doubleTwice(5)) // quadruples a number because it doubles it and then doubles it again, like double(double(5))
