2168becbf3bd45c4983114be0464d6a42271280e
tj-commits <thefoxjones@gmail.com>
2026-04-10 09:35:24 -0500

    revert to 1.2.0 logic

--- index.js
+++ index.js
@@ -2,20 +2,8 @@
 
 var composeFunction = require("compose-function")
 var flatmap = require("array.prototype.flatmap")
-var $apply = require("function.apply-x")
-var arrayWrap = require("as-array")
-var times = require("lolite.__private.multiplyfallback")
-var numberTwo = require("es-two")
-var constant = require("literally")
-var call = require("call-bind-enterprise/src/callBoundCall")
-var nullvalue = require("empty-module")
 
 // eslint-disable-next-line unicorn/prevent-abbreviations
 module.exports = function twiceCallWrapper(fn) {
-  return call(
-    $apply,
-    composeFunction,
-    nullvalue,
-    flatmap(arrayWrap(fn), item => times(numberTwo, constant(item))),
-  )
+  return composeFunction(...flatmap([fn], item => [item, item]))
 }
